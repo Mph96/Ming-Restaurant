aad9a1fd16cfd1478cf5211d96d838aeeb645123
Mph96 <matthew.hernandez@hcl.com>
2021-02-20 21:45:27 -0800

    First Commit

---
+++ MingRestaurant/src/main/java/com/hcl/MingRestaurant/MingRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.MingRestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MingRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MingRestaurantApplication.class, args);
+	}
+
+}
---
+++ MingRestaurant/src/test/java/com/hcl/MingRestaurant/MingRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.MingRestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MingRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
